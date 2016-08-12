324152af1f8bf1aad49dddbf0cbe8a71d94568b9
raunaak <rklohiya1996@gmail.com>
2016-08-12 13:02:20 +0530

    Add function to count dearrangements
    Question Link : http://www.geeksforgeeks.org/count-derangements-permutation-such-that-no-element-appears-in-its-original-position/

---
+++ CountDearrangement.h
@@ -0,0 +1,17 @@
+#ifndef COUNTDEARRANGEMENT_H
+#define COUNTDEARRANGEMENT_H
+//Question Link : http://www.geeksforgeeks.org/count-derangements-permutation-such-that-no-element-appears-in-its-original-position/
+//Formula : D(n) = (n-1)*(D(n-1)+D(n-2))
+
+int countDearrangement(int n){
+    int count[n+1];
+    count[0] = 0;
+    count[1] = 0;
+    count[2] = 1;
+    for(int i=3; i<=n; i++)count[i]=(i-1)*(count[i-1]+count[i-2]);
+    return count[n];
+}
+
+
+#endif /* COUNTDEARRANGEMENT_H */
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "CountDearrangement.h"
+using namespace std;
+
+int main(){
+    cout<<countDearrangement(4);
+    return 0;
+}
+
